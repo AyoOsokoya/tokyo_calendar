c64ed1898f3a0b6a79ef9bbe76c9c4493cc9e20b
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-12-05 23:46:01 +0900

    Start laying out route structure

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,75 @@ use App\Http\Controllers\EventController;
 |
 */
 
-Route::prefix('/v1')
-    ->group(function () {
+Route::prefix('/v1')->group(function () {
     Route::get('/events/', [EventController::class, 'allEvents']);
     Route::get('/user/events/{response_format?}', [EventController::class, 'userEvents']);
-    Route::get('/user/events/attendance_status/{attendance_status}/{response_format?}',
-        [EventController::class, 'userEventsByAttendanceStatus']
-    );
+    Route::get('/user/events/attendance_status/{attendance_status}/{response_format?}', [EventController::class, 'userEventsByAttendanceStatus'] );
 
-    // /user/events/now
-    // /user/events/recommended
-    // /user/events/weekend
-    // /user/events/history
-    // /user/events/soon
-    // /user/events/interest/soon
-    // /user/events/curious/
+    Route::prefix('/user')->group(function () {
+        // /events/
+        // /events/future
+        // /events/history
+
+        // /follow/{user_id}
+        // /unfollow/{user_id}
+        // /following
+        // /followers
+
+        // /space/follow/{space_id}
+        // /space/unfollow/{space_id}
+
+        // lists
+        Route::prefix('/list')->group(function () {
+            // /create
+            // /update (add or remove people), change time etc
+            // /view/{list_id}
+            // /delete/{list_id}
+            // /list/share_event/{list_id}/{event_id}
+        });
+        Route::get('lists', function () {
+            return response()->json(['message' => 'Hello World!']);
+        });
+    });
+
+    Route::prefix('/space')->group(function () {
+        // /create
+        // /update/{space_id}
+        // /view/{space_id}
+        // /delete/{space_id}
+        // /user/remove/{space_id}
+    });
+
+    Route::prefix('/event')->group(function () {
+        // /create
+        // /update/{event_id}
+        // /view/{event_id}
+        // /delete/{event_id}
+    });
+
+    Route::get('/events', function () {
+        return response()->json(['message' => 'Hello World!']);
+        // now, recommended, weekend, history, soon, interest, curious
+        // by category etc
+        // search
+    });
+
+    Route::prefix('/community')->group(function () {
+        // /create
+        // /update/{community_id}
+        // /view/{community_id}
+        // /delete/{community_id}
+        // /archive/{community_id}
+        // /unarchive/{community_id}
+        // /user/invite/{community_id}
+        // /user/remove/{community_id}
+    });
+    Route::get('/communities', function () {
+        return response()->json(['message' => 'Hello World!']);
+    });
 });
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
