593f5ce99b8afdc8246fabc8c6bb7631d7b56571
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-12-14 12:32:38 +0900

    Add softd eletes.

--- app/Domains/Users/Models/UserSpace.php
+++ app/Domains/Users/Models/UserSpace.php
@@ -1,12 +1,15 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Domains\Users\Models;
 
 use App\Domains\Spaces\Models\Space;
 use App\Domains\Users\Enums\EnumUserSpaceInviteStatus;
 use App\Domains\Users\Enums\EnumUserSpaceRoleType;
+use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 /**
@@ -21,10 +24,11 @@ use Illuminate\Support\Carbon;
  * @property Carbon $created_at
  * @property Carbon $updated_at
  * @property Carbon $deleted_at
+ * @mixin Eloquent
  */
 class UserSpace extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'user_spaces';
 
     protected $fillable = [
