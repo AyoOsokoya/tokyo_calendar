bb3949d8875b1463b4c3c227f119fbe17830e98c
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-12-15 20:25:52 +0900

    Socials Enum

---
+++ app/Domains/Socials/Enums/EnumSocials.php
@@ -0,0 +1,55 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domains\Socials\Enums;
+
+enum EnumSocials: string
+{
+    case ANGELLIST = 'angellist';
+    case APPSTORE = 'appstore';
+    case BANDCAMP = 'bandcamp';
+    case BEHANCE = 'behance';
+    case BITBUCKET = 'bitbucket';
+    case CODEPEN = 'codepen';
+    case DISCORD = 'discord';
+    case DRIBBBLE = 'dribbble';
+    case FACEBOOK = 'facebook';
+    case FLICKR = 'flickr';
+    case FOURSQUARE = 'foursquare';
+    case GITHUB = 'github';
+    case GOODREADS = 'goodreads';
+    case GOOGLEPLUS = 'googleplus';
+    case INSTAGRAM = 'instagram';
+    case ITUNES = 'itunes';
+    case KICKSTARTER = 'kickstarter';
+    case LASTFM = 'lastfm';
+    case LINE = 'line';
+    case LINKEDIN = 'linkedin';
+    case MEDIUM = 'medium';
+    case MEETUP = 'meetup';
+    case MIXER = 'mixer';
+    case PATREON = 'patreon';
+    case PINTEREST = 'pinterest';
+    case PLAYSTORE = 'playstore';
+    case QUORA = 'quora';
+    case REDDIT = 'reddit';
+    case SLACK = 'slack';
+    case SNAPCHAT = 'snapchat';
+    case SOUNDCLOUD = 'soundcloud';
+    case SPOTIFY = 'spotify';
+    case STACKOVERFLOW = 'stackoverflow';
+    case STEAM = 'steam';
+    case TELEGRAM = 'telegram';
+    case TIKTOK = 'tiktok';
+    case TRIP = 'trip';
+    case TRIPADVISOR = 'tripadvisor';
+    case TRUSTPILOT = 'trustpilot';
+    case TUMBLR = 'tumblr';
+    case TWITCH = 'twitch';
+    case TWITTER = 'twitter';
+    case VIMEO = 'vimeo';
+    case VINE = 'vine';
+    case WHATSAPP = 'whatsapp';
+    case YELP = 'yelp';
+    case YOUTUBE = 'youtube';
+}
