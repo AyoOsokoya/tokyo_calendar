538c2eba595381b06fe567e2cabfc9ec62bdd348
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-12-18 19:49:24 +0900

    Fix formatting.

--- app/Domains/Users/Models/Tables/TableUser.php
+++ app/Domains/Users/Models/Tables/TableUser.php
@@ -25,8 +25,11 @@ class TableUser
     public const email = 'email';
 
     public const user_role_type = 'user_role_type';
+
     public const account_status = 'account_status';
+
     public const account_type = 'account_type';
+
     public const email_verified_at = 'email_verified_at';
 
     public const password = 'password';
