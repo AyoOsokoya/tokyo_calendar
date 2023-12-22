72b4f716b19e1736568249640e0def8763e6f1a7
Ayo Osokoya <ayo.osokoya@gmail.com>
2023-12-23 02:07:25 +0900

    Create staff admin

--- tests/Unit/Domains/Users/Models/UserTest.php
+++ tests/Unit/Domains/Users/Models/UserTest.php
@@ -7,7 +7,9 @@ namespace Tests\Unit\Domains\Users\Models;
 use App\Domains\Events\Models\Event;
 use App\Domains\Location\Models\Location;
 use App\Domains\Spaces\Models\Space;
+use App\Domains\Users\Enums\EnumUserStaffRole;
 use App\Domains\Users\Models\User;
+use App\Domains\Users\Models\Tables\TableUser as _;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -48,7 +50,7 @@ class UserTest extends TestCase
             // Create one staff Admin (user 1)
             // Create one space admin for each space space 2 admin = user 2, space 3 admin = user 3 etc
             // Create 100 users who are just default users
-                // follow PSEUDO random events and spaces
+                // follow PSEUDO random events and spaces (srand())
             // Follow 25 spaces each
             // Follow 25 events each
             // Follow 25 friends and make follow backs (do this in the specific test)
@@ -56,14 +58,27 @@ class UserTest extends TestCase
         // Can spaces follow other spaces ???
 
         // 100 users
+        $staff_admin = User::factory()->state([
+            _::name_first => 'Admin',
+            _::name_middle => 'Test',
+            _::name_last => 'User',
+            _::name_handle => 'admin',
+            _::email => 'admin@user.com',
+            _::staff_role => EnumUserStaffRole::ADMIN->value,
+        ])->create();
+
+        $users = User::factory()->count(100)->create();
+        $spaces = Space::factory()->count(10)->create();
         // 10 spaces
-        // 10 events per space
+        // 15 events per space
             // some in the past
             // some in the future
-            // some now
+            // some happening now
         // Users follow spaces
+        $users->each(function (User $user) use ($spaces) {
+            $user->spaces()->attach($spaces->random(25));
+        });
         // users follow/attend events
-
 
         User::factory()->has(
             Space::factory()
