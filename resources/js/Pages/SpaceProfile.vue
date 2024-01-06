6fd8c0640afb672f970941ca1d922ce6482b86a3
Ayo Osokoya <ayo.osokoya@gmail.com>
2024-01-06 21:03:56 +0000

    Rename page

---
+++ resources/js/Pages/SpaceProfile.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import GenericLayout from "@/Layouts/GenericLayout.vue";
+</script>
+
+<template>
+    <GenericLayout>
+        <!-- Profile Sale / Pitch Header (The follow it pitch) -->
+            <!-- HOW CAN I CONDENSE ALL OF THIS-->
+            <!-- Pinned Events -->
+            <!-- Follower information (mutuals, total, follows) -->
+            <!-- Avatar -->
+            <!-- Schedule -->
+            <!-- Name -->
+            <!-- location/Directions -->
+            <!-- Description -->
+            <!-- Gallery -->
+
+        <!-- Profile Main (The come and visit asap pitch) -->
+            <!-- Events -->
+                <!-- Soon/Now/Next/ in X hours/ tonight -->
+                <!-- Past events -->
+                <!-- Occupancy / how busy -->
+                <!-- "Shouts" of news information etc -->
+            <!-- Call out -->
+
+<!--    <div class="flex flex-col items-center justify-center min-h-screen py-2">-->
+<!--        <div class="flex flex-col items-center justify-center">-->
+<!--            <h1 class="text-6xl font-bold">-->
+<!--                Welcome to your profile space-->
+<!--            </h1>-->
+<!--            <p class="mt-3 text-2xl">-->
+<!--                This is your profile space. You can edit your profile here.-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </div>-->
+    </GenericLayout>
+</template>
+
+<style scoped>
+
+</style>
